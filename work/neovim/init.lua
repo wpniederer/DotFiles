4d1a49230590e4340a180044188e61d31ef28c7f
wpniederer <wpniederer@gmail.com>
2025-03-06 12:34:18 -0800

    Add keymaps for MarkdownPreview

--- work/neovim/init.lua
+++ work/neovim/init.lua
@@ -189,6 +189,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- For MarkdownPreview
+vim.keymap.set('n', '<C-m>', '<Plug>MarkdownPreview', { desc = 'Enable MarkdownPreview' })
+vim.keymap.set('n', '<C-s>', '<Plug>MarkdownPreviewStop', { desc = 'Stop MarkdownPreview' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
